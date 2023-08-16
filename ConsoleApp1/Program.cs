0816f0fb3bca3bce0565ad89121b733a57ce5051
posey <posey@DESKTOP-KEAP1HJ>
2023-08-16 19:46:42 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿/*using System;
+
+class Program
+{
+    static void Main()
+    {
+        string fullName = "Гафуров Малик";
+        int age = 21;
+        string email = "poseydon100500@gmail.com";
+        double programmerScore = 55.5;
+        double mathScore = 50.5;
+        double physicScore = 54.3;
+        Console.WriteLine($"ФИО: {fullName}, \nВозраст: {age}, \nEmail:{email}, \nБаллы по программированию: {programmerScore}, \nБаллы по математике: {mathScore}, \nБаллы по физике: {physicScore}");
+        Console.ReadKey();
+    }
+}*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string fullName = "Гафуров Малик";
+        int age = 21;
+        string email = "poseydon100500@gmail.com";
+        double programmerScore = 55.5;
+        double mathScore = 50.5;
+        double physicScore = 54.3;
+
+        double totalScore = programmerScore + mathScore + physicScore;
+        double averageScore = totalScore / 3;
+        Console.WriteLine($"ФИО: {fullName}.\nВозраст: {age}. \nEmail: {email}. \nБаллы по программированию: {programmerScore}. \nБаллы по математике: {mathScore}. \nБаллы по физике: {physicScore}. \nСумма всех баллов: {totalScore}. \nСредний балл: {averageScore}");
+        Console.ReadKey();
+    }
+}
